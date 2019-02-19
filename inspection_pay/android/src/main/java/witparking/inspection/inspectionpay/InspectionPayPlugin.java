e2d8dc745371e2d901227b203ba926040bee3492
杨玉玺 <yangyuxikaca@163.com>
2019-02-19 15:39:07 +0800

    s

--- inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
+++ inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
@@ -49,7 +49,7 @@ public class InspectionPayPlugin implements MethodCallHandler {
 
   private void unionPay(MethodCall call, Result result) {
 
-    int a = call.argument("appId");
+    int a = call.argument("appId") == null ? 0 : (int) call.argument("amt");
 
     JSONObject transData = new JSONObject();
     try {
