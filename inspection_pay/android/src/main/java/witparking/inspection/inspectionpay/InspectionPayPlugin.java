d6b2752bf049b797fccca883a7c1504c29018069
杨玉玺 <yangyuxikaca@163.com>
2019-02-19 11:29:14 +0800

    0.0.3

--- inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
+++ inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
@@ -1,5 +1,7 @@
 package witparking.inspection.inspectionpay;
 
+import android.content.pm.ApplicationInfo;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -9,11 +11,15 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
+import com.ums.AppHelper;
+
 /** InspectionPayPlugin */
 public class InspectionPayPlugin implements MethodCallHandler {
 
   private static final String SCANTOPAY = "pay_scan";
   private static final String UNIONPAY = "pay_union";
+
+  private static final String WUWEIUNIONAPPID = "e7e157a1475e453ea82d17b4f9184551";
 
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
@@ -32,6 +38,17 @@ public class InspectionPayPlugin implements MethodCallHandler {
       } catch (JSONException e) {
         e.printStackTrace();
       }
+
+      JSONObject transData = new JSONObject();
+      try {
+        transData.put("appId", WUWEIUNIONAPPID);
+        transData.put("amt", 0.01);
+      } catch (JSONException e) {
+        e.printStackTrace();
+      }
+
+      AppHelper.callTrans(null, "POS 通", "扫一扫", transData);
+      //AppHelper.callTrans(cordova.getActivity(), "银行卡收款", "消费", transData);
 
       result.success("--------");
 
