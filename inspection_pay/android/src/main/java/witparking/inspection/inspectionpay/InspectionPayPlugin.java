c91f70697d67296a30135aedd55a96a6fd3be2b6
杨玉玺 <yangyuxikaca@163.com>
2019-02-19 15:35:57 +0800

    s

--- inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
+++ inspection_pay/android/src/main/java/witparking/inspection/inspectionpay/InspectionPayPlugin.java
@@ -19,7 +19,7 @@ import com.ums.AppHelper;
 public class InspectionPayPlugin implements MethodCallHandler {
 
   private static final String SCANTOPAY = "pay_scan";
-  private static final String UNIONPAY = "pay_union_scan";
+  private static final String UNIONPAYSCAN = "pay_union_scan";
 
   private static final String WUWEIUNIONAPPID = "e7e157a1475e453ea82d17b4f9184551";
 
@@ -38,10 +38,8 @@ public class InspectionPayPlugin implements MethodCallHandler {
   public void onMethodCall(MethodCall call, Result result) {
 
     switch (call.method) {
-      case UNIONPAY:
-        unionPay(result, call.arguments);
-        break;
-      case SCANTOPAY:
+      case UNIONPAYSCAN:
+        unionPay(call, result);
         break;
       default:
         result.notImplemented();
@@ -49,13 +47,9 @@ public class InspectionPayPlugin implements MethodCallHandler {
     }
   }
 
-  private void unionPay(Result result, Object param) {
-    JSONObject jsonObject = new JSONObject();
-    try {
-      jsonObject.put("test", "AAAA");
-    } catch (JSONException e) {
-      e.printStackTrace();
-    }
+  private void unionPay(MethodCall call, Result result) {
+
+    int a = call.argument("appId");
 
     JSONObject transData = new JSONObject();
     try {
@@ -70,7 +64,8 @@ public class InspectionPayPlugin implements MethodCallHandler {
       //AppHelper.callTrans(cordova.getActivity(), "银行卡收款", "消费", transData);
     } catch (Exception e) {
       result.success("未安装银联客户端");
+      return;
     }
-    result.success("支付成功");
+    //result.success("支付成功");
   }
 }
