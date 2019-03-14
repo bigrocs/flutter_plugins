042373d50b21b9a8d3f80cfdd7e1dc4aa7cdf1e3
杨玉玺 <yangyuxikaca@163.com>
2019-03-14 11:02:57 +0800

    ocr

--- inspection_update/android/src/main/java/witparking/inspection/inspectionupdate/InspectionUpdatePlugin.java
+++ inspection_update/android/src/main/java/witparking/inspection/inspectionupdate/InspectionUpdatePlugin.java
@@ -8,8 +8,6 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** InspectionUpdatePlugin */
 public class InspectionUpdatePlugin implements MethodCallHandler {
-
-
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "inspection_update");
@@ -18,7 +16,7 @@ public class InspectionUpdatePlugin implements MethodCallHandler {
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("update")) {
+    if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
     } else {
       result.notImplemented();
--- inspection_update/lib/inspection_update.dart
+++ inspection_update/lib/inspection_update.dart
@@ -6,8 +6,8 @@ class InspectionUpdate {
   static const MethodChannel _channel =
       const MethodChannel('inspection_update');
 
-  static Future platformVersion() async {
-    dynamic res = await _channel.invokeMethod('update');
-    return res;
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
   }
 }
