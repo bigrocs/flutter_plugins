dc1d0d3b05a6ed898ebce1eec51b452ce539255b
杨玉玺 <yangyuxikaca@163.com>
2019-04-28 15:33:08 +0800

    更新

--- inspection_update/android/src/main/java/witparking/inspection/inspectionupdate/InspectionUpdatePlugin.java
+++ inspection_update/android/src/main/java/witparking/inspection/inspectionupdate/InspectionUpdatePlugin.java
@@ -1,10 +1,16 @@
 package witparking.inspection.inspectionupdate;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 
-import java.util.Date;
+import com.ypy.eventbus.EventBus;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -16,6 +22,14 @@ public class InspectionUpdatePlugin implements MethodCallHandler {
 
   static Registrar registrar;
 
+  private static EventChannel.EventSink eventSink;
+  private Result updateResult;
+
+
+  InspectionUpdatePlugin() {
+    EventBus.getDefault().register(this);
+  }
+
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
 
@@ -23,6 +37,19 @@ public class InspectionUpdatePlugin implements MethodCallHandler {
 
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "inspection_update");
     channel.setMethodCallHandler(new InspectionUpdatePlugin());
+
+    final EventChannel message_channel = new EventChannel(registrar.messenger(), "update.event.progress");
+    message_channel.setStreamHandler(new EventChannel.StreamHandler() {
+      @Override
+      public void onListen(Object o, EventChannel.EventSink eventSink) {
+        InspectionUpdatePlugin.eventSink = eventSink;
+      }
+
+      @Override
+      public void onCancel(Object o) {
+        InspectionUpdatePlugin.eventSink = null;
+      }
+    });
   }
 
   @Override
@@ -31,13 +58,33 @@ public class InspectionUpdatePlugin implements MethodCallHandler {
     * 更新APP
     * */
     if (call.method.equals("update")) {
+      updateResult = result;
       String url = call.argument("url");
       AppUpdateUtils appUpdateUtils = new AppUpdateUtils(InspectionUpdatePlugin.registrar.activity());
-      //String fileName = "WP" + new Date().getTime() + ".apk";
       String fileName = "巡检端.apk";
       appUpdateUtils.downloadAppWithUrl(url, fileName);
+
     } else {
       result.notImplemented();
+    }
+  }
+
+  public void onEventMain(AppUpdateEvent event) {
+    if (event.error.equals("")) {
+      Map map = new HashMap();
+      map.put("schedule", event.progress);
+      map.put("carryOut", event.complete);
+      if (InspectionUpdatePlugin.eventSink != null) {
+        InspectionUpdatePlugin.eventSink.success(map);
+      }
+      if (event.progress >= 100) {
+        updateResult.success("done");
+      }
+    }else
+    {
+      if (InspectionUpdatePlugin.eventSink != null) {
+        InspectionUpdatePlugin.eventSink.error("0", event.error, event.error);
+      }
     }
   }
 }
--- inspection_update/lib/inspection_update.dart
+++ inspection_update/lib/inspection_update.dart
@@ -3,11 +3,31 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class InspectionUpdate {
+
+
   static const MethodChannel _channel =
       const MethodChannel('inspection_update');
 
-  static Future update(String url) async {
+  static Future update(String url, Function callback) async {
+    StreamSubscription streamSubscription = _subscribeUpdateProgress(callback: callback);
     final dynamic res = await _channel.invokeMethod('update', {'url' : url});
+    if (res == 'done') {
+      _unSubscribeUpdateProgress(streamSubscription);
+    }
     return res;
+  }
+
+  /*
+  * 更新进度
+  * */
+  static const EventChannel _stream = const EventChannel('update.event.progress');
+
+  static StreamSubscription _subscribeUpdateProgress({Function callback}) {
+    return _stream.receiveBroadcastStream().listen(callback);
+  }
+
+  static _unSubscribeUpdateProgress(StreamSubscription streamSubscription) {
+    streamSubscription.cancel();
+    streamSubscription = null;
   }
 }
